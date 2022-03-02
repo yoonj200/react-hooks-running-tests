dd5c00dea84a220ae1ba25a79d074c8c1d583878
Joshua <yoonj200@gmail.com>
2022-03-02 11:43:16 -0600

    Completed assignment

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,11 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  screen.debug();
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  
+  // You can also use this to debug a single element
+  // const element = screen.queryByText("please pass this test");
+  // screen.debug(element);
+  // expect(element).toBeInTheDocument();
 });
